ab6f91dffb28548bdd715c7e2533c63a0c0a661d
yuta.yoshioka <yuta.yoshi.reps15@gmail.com>
2026-03-14 21:33:31 +0900

    認証関連の画面の大枠を作成

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/base.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/layout.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/components.css') }}">
+
+    {{-- Page CSS --}}
+    @yield('css')
+</head>
+
+<body>
+    <div class="app">
+        <header class="header">
+            <a href="@yield('header-logo-link')" class="header__logo">
+                <img src="{{ asset('images/coachtech-header-logo.png') }}" alt="COACHTECH">
+            </a>
+            @yield('header-nav')
+        </header>
+        <main class="content">
+            @yield('content')
+        </main>
+    </div>
+</body>
+</html>
---
+++ src/resources/views/user/auth/register.blade.php
@@ -0,0 +1,66 @@
+{{-- Layout --}}
+@extends('layouts.app')
+
+{{-- Title --}}
+@section('title', '会員登録画面（一般ユーザー）')
+
+{{-- Header --}}
+@section('header-logo-link', url('/login'))
+
+{{-- CSS --}}
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/auth.css') }}">
+@endsection
+
+{{-- Content --}}
+@section('content')
+<div class="auth-content">
+    <div class="auth-form">
+        <h1 class="auth-form__heading form-heading">会員登録</h1>
+
+        <div class="auth-form__inner">
+            <form action="/register" method="post" class="auth-form__form" novalidate>
+                @csrf
+
+                <div class="auth-form__group">
+                    <label for="name" class="auth-form__label">ユーザー名</label>
+                    <input type="text" name="name" id="name" value="{{ old('name') }}" class="auth-form__input">
+                    @error('name')
+                    <p class="auth-form__error-message">{{ $message }}</p>
+                    @enderror
+                </div>
+
+                <div class="auth-form__group">
+                    <label for="email" class="auth-form__label">メールアドレス</label>
+                    <input type="email" name="email" id="email" value="{{ old('email') }}" class="auth-form__input">
+                    @error('email')
+                    <p class="auth-form__error-message">{{ $message }}</p>
+                    @enderror
+                </div>
+
+                <div class="auth-form__group">
+                    <label for="password" class="auth-form__label">パスワード</label>
+                    <input type="password" name="password" id="password" class="auth-form__input">
+                    @error('password')
+                    <p class="auth-form__error-message">{{ $message }}</p>
+                    @enderror
+                </div>
+
+                <div class="auth-form__group">
+                    <label for="password_confirmation" class="auth-form__label">パスワード確認</label>
+                    <input type="password" name="password_confirmation" id="password_confirmation" class="auth-form__input">
+                    @error('password_confirmation')
+                    <p class="auth-form__error-message">{{ $message }}</p>
+                    @enderror
+                </div>
+
+                <div>
+                    <input type="submit" value="登録する" class="auth-form__btn btn">
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <a href="/login" class="auth-content__link">ログインはこちら</a>
+</div>
+@endsection
