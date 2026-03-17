d64be601874fd8b8c1355de75bedf343819c6664
yuta.yoshioka <yuta.yoshi.reps15@gmail.com>
2026-03-17 23:58:23 +0900

    確認用パスワードのバリデーション内容をコーチに確認し修正

--- src/app/Http/Requests/RegisterRequest.php
+++ src/app/Http/Requests/RegisterRequest.php
@@ -25,7 +25,7 @@ class RegisterRequest extends FormRequest
             'name' => ['required'],
             'email' => ['required', 'email'],
             'password' => ['required', 'min:8'],
-            'password_confirmation' => ['required', 'min:8', 'same:password'],
+            'password_confirmation' => ['same:password'],
         ];
     }
 
@@ -37,8 +37,6 @@ class RegisterRequest extends FormRequest
             'email.email' => 'メールアドレスはメール形式で入力してください',
             'password.required' => 'パスワードを入力してください',
             'password.min' => 'パスワードは8文字以上で入力してください',
-            'password_confirmation.required' => '確認用パスワードを入力してください',
-            'password_confirmation.min' => '確認用パスワードは8文字以上で入力してください',
             'password_confirmation.same' => 'パスワードと一致しません',
         ];
     }
