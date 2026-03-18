d1f2a6efaf267339505ff41f44a21963489932bd
yuta.yoshioka <yuta.yoshi.reps15@gmail.com>
2026-03-19 01:07:19 +0900

    勤怠登録画面のドラフト作成

--- src/resources/views/user/attendance/index.blade.php
+++ src/resources/views/user/attendance/index.blade.php
@@ -19,5 +19,37 @@
 
 {{-- Content --}}
 @section('content')
-<h1>勤怠登録画面（一般ユーザー）</h1>
+<div class="attendance-content">
+    <span class="attendance-status">{{ $status }}</span>
+    <span class="attendance-date">{{ $date }}</span>
+    <span class="attendance-time">{{ $time }}</span>
+    <div class="attendance-form">
+        @switch($status)
+            @case('勤務外')
+                <form action="/attendance/clock-in" method="post" class="attendance-form__form">
+                    @csrf
+                    <button type="submit" class="attendance-form__btn">出勤</button>
+                </form>
+                @break
+            @case('出勤中')
+                <form action="/attendance/clock-out" method="post" class="attendance-form__form">
+                    @csrf
+                    <button type="submit" class="attendance-form__btn">退勤</button>
+                </form>
+                <form action="/attendance/break-start" method="post" class="attendance-form__form">
+                    @csrf
+                    <button type="submit" class="attendance-form__btn attendance-form__btn--break">休憩入</button>
+                </form>
+                @break
+            @case('休憩中')
+                <form action="/attendance/break-end" method="post" class="attendance-form__form">
+                    @csrf
+                    <button type="submit" class="attendance-form__btn attendance-form__btn--break">休憩戻</button>
+                </form>
+                @break
+            @default
+                <span class="attendance-form__clock-out">お疲れ様でした。</span>
+        @endswitch
+    </div>
+</div>
 @endsection
