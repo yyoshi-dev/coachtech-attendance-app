4566c9c70d0642501afc117bd284f7cb3aefc4b1
yuta.yoshioka <yuta.yoshi.reps15@gmail.com>
2026-04-25 21:03:53 +0900

    使っていないfeaturesを削除

--- src/config/fortify.php
+++ src/config/fortify.php
@@ -145,15 +145,7 @@ return [
 
     'features' => [
         Features::registration(),
-        Features::resetPasswords(),
         Features::emailVerification(),
-        Features::updateProfileInformation(),
-        Features::updatePasswords(),
-        Features::twoFactorAuthentication([
-            'confirm' => true,
-            'confirmPassword' => true,
-            // 'window' => 0,
-        ]),
     ],
 
 ];
